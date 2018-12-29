2980049795a5cec74a652a89b0ab6a06ffabe984
Mycah <mycahkrason@gmail.com>
2018-12-29 13:49:04 -0800

    Added token method and Post /users

--- server/models/user.js
+++ server/models/user.js
@@ -1,13 +1,60 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-//Create a user model
-var User = mongoose.model('User', {
+
+//user a schema so we can add on customer methods
+var userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
         trim: true,
-        minlength: 1
-    }
+        minlength: 1, 
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid Email'
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
+
+//We need to bind 'this', so don't use an arrow function
+userSchema.methods.toJSON = function(){
+    var user = this;
+    var userObject = user.toObject();
+    
+    return _.pick(userObject, ['_id', 'email']);
+};
+
+userSchema.methods.generateAuthToken = function(){
+    var user = this;
+    var access = 'auth';
+    var token = jwt.sign({_id: user._id.toHexString(), access}, 'randomsecret').toString();
+    
+    user.tokens = user.tokens.concat([{access, token}]);
+    
+    return user.save().then(() => {
+        return token;
+    });
+};
+
+//Create a user model
+var User = mongoose.model('User', userSchema);
 
 module.exports = {User};
